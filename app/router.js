e391d60c25afc42949c57280f8d72a800aeea022
Emanuel Coelho <emanuelcoelho1986@gmail.com>
2021-07-14 22:17:02 +0100

    pilot[layout] Simple layout init

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('home', { path: '/' });
+  this.route('blog');
+  this.route('about');
+  this.route('socials');
+});
