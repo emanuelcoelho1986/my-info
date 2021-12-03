a91566d111cdf826abc39f6e5009e9f250b098e9
Emanuel Coelho <emanuelcoelho1986@users.noreply.github.com>
2021-12-03 11:33:22 +0000

    added[education] information from JSON (#14)

--- app/routes/education.js
+++ app/routes/education.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default class EducationRoute extends Route {}
+export default class EducationRoute extends Route {
+  async model() {
+    const response = await fetch('/data/education.json');
+    const experiences = await response.json();
+
+    return experiences;
+  }
+}
