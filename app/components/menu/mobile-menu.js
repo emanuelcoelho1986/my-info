cf4a6a6c759fd319653eac889212533d32fcf826
Emanuel Coelho <emanuelcoelho1986@users.noreply.github.com>
2021-12-04 15:46:34 +0000

    Improve mobile styles and related issues (#16)
    * improve[mobile] styles
    
    * fix[mobile-menu] navigation

--- app/components/menu/mobile-menu.js
+++ app/components/menu/mobile-menu.js
@@ -1,49 +1,19 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { keyResponder, onKey } from 'ember-keyboard';
 
-@keyResponder
 export default class MenuMobileMenuController extends Component {
   @tracked isOpen = false;
 
-  didRenderElement = () => {
-    window.addEventListener('keydown', (event) =>
-      this.shouldPerformMenuCheck(event)
-    );
-  };
-
-  willDestroy() {
-    super.willDestroy();
-
-    window.removeEventListener('keydown', (event) =>
-      this.shouldPerformMenuCheck(event)
-    );
+  @action
+  didChangeRoute() {
+    this.setMenu(false);
   }
 
   @action
   didPressMenuButton() {
     this.setMenu(!this.isOpen);
   }
-
-  // I hate this approach
-  @onKey('escape')
-  shouldPerformMenuCheck = (event) => {
-    switch (event?.key.toLowerCase()) {
-      case 'escape':
-        this.setMenu(false);
-        break;
-      case 'm':
-        this.setMenu(!this.isOpen);
-        break;
-      default:
-        console.info('Trying to do something unexpected');
-    }
-
-    if (event && event?.key.toLowerCase().indexOf('escape') > -1) {
-      this.isOpen = false;
-    }
-  };
 
   setMenu(isOpen) {
     this.isOpen = isOpen;
