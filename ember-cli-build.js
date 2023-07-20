083fe29a08654f3e940e6a117f013de8c779c3ce
Emanuel Coelho <emanuelcoelho1986@gmail.com>
2023-07-20 18:15:08 +0100

    restore[settings] in some dotfiles

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
-    LOG_TRANSITIONS: true,
   });
 
   return app.toTree();
